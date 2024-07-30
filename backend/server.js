f8ac634fda37f9c5332d611b3cb228b27e385e03
insidepixce <insidepixce@gmail.com>
2024-07-30 13:28:34 +0900

    주석 달기

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-// MongoDB 연결 설정 (환경 변수 사용)
+// MongoDB 연결 설정 (환경 변수 사용 -> dotenv 파일 확실히 체크할것 
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
@@ -22,10 +22,10 @@ app.use('/api/photoPost', photoPostRoutes);
 app.use('/api/stopwatch', stopwatchRoutes);
 app.use('/api/board', boardRoutes);
 
-// 정적 파일 제공
+// 정적 파일 제공 -> 프론트앤드 파일에 올림
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-// 업로드된 파일 제공
+// 업로드된 파일 로딩 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 3000;
