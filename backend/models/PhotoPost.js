630efeca80b10856acdea8ca6bf962a4c74bcc68
insidepixce <insidepixce@gmail.com>
2024-07-31 14:44:00 +0900

    주석 추가

--- backend/models/PhotoPost.js
+++ backend/models/PhotoPost.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const photoPostSchema = new mongoose.Schema({
+    //이미지를 저장하는 필드 
     image: String,
+    //이미지에 대한 코멘트를 저장하는 필드 
     comment: String
 });
 
