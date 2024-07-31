8a9a4538f9075dfbf883fc06f36f11f54c0383ab
insidepixce <insidepixce@gmail.com>
2024-07-31 14:43:13 +0900

    주석 추가

--- backend/models/Stopwatch.js
+++ backend/models/Stopwatch.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-
+//스톱워치 로그 저장에 대한 스키마 
 const stopwatchSchema = new mongoose.Schema({
+    //로그 항목 저장 필드, 숫자(함수) 
     logEntry: { type: Number, required: true },
+    //메모 저장 필드 
     memo: { type: String, default: '' },
+    //이미지 저장 필드 
     image: { type: String, default: '' }
 });
 
